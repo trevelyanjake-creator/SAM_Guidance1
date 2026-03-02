a05b45491a07fd915f144d30a83657fa9ab3b3ac
trevelyanjake-creator <trevelyanjake@gmail.com>
2026-03-02 22:31:07 +1100

    Update Target.py

--- SAM/Target.py
+++ SAM/Target.py
@@ -21,10 +21,46 @@ class Target:
 
         Target_VXscatter = 0
         Target_VYscatter = 30
-        Target_VZscatter = 0
+        Target_VZscatter = -5
 
         self.Target_state = np.array([Target_Xscatter, Target_Yscatter, Target_Zscatter], dtype=float)
         self.Target_Vstate = np.array([Target_VXscatter, Target_VYscatter, Target_VZscatter], dtype=float)
 
         (self.Target,) = ax.plot([self.Target_state[0]], [self.Target_state[1]], [self.Target_state[2]], marker='o', color = 'blue')
         (self.Tpath,) = ax.plot(self.Target_Path[0], self.Target_Path[1], self.Target_Path[2], color = 'blue')
+    
+    def targetGuidance(self, dt):
+        enforce = False
+        if self.Target_state[0] < 50:
+            self.Target_Vstate[0] += 0.5 * (50 - self.Target_state[0])/50
+            self.Target_Vstate[1] = self.Target_Vstate[1] * 1.01
+            enforce = True
+        elif self.Target_state[0] > 450:
+            self.Target_Vstate[0] += -0.5 * (self.Target_state[0] - 450)/50
+            self.Target_Vstate[1] = self.Target_Vstate[1] * 1.01
+            enforce = True
+        if self.Target_state[2] < 20:
+            self.Target_Vstate[2] += 0.5 * (20 - self.Target_state[2])/20
+            self.Target_Vstate[1] = self.Target_Vstate[1] * 1.01
+            enforce = True
+        elif self.Target_state[2] > 300:
+            self.Target_Vstate[2] += -0.5 * (self.Target_state[2] - 300)/50
+            self.Target_Vstate[1] = self.Target_Vstate[1] * 1.01
+            enforce = True
+
+        if enforce == False:
+            axisChanging = np.random.randint(0, 3)
+            Delta = np.random.default_rng().uniform(low=-1, high=1)
+
+            self.Target_Vstate[axisChanging] += Delta
+
+        v_norm = np.linalg.norm(self.Target_Vstate)
+        v_max = 35
+        if v_norm > v_max and v_norm > 0:
+            self.Target_Vstate *= v_max / v_norm
+
+        self.Target_state += self.Target_Vstate * dt
+
+        self.Target_Path[0].append(float(self.Target_state[0]))
+        self.Target_Path[1].append(float(self.Target_state[1]))
+        self.Target_Path[2].append(float(self.Target_state[2]))
